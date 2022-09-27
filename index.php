e6b8244914e8e1705d17332749678dbd961fc5a2
DouglasOliveira <douglasoliveiradaluz10@gmail.com>
2022-09-27 12:22:59 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -9,9 +9,8 @@
 </head>
 
 <header>
-
+<img class="./img/logo-justica-eleitora-senac.png">
 </header>
-
 <body>
 
 <div class="retangulo1">
@@ -19,5 +18,38 @@
         <div class="retangulo3"></div>
     </div>
 </div>
+
+<div class="preto1">
+    <button class="btn click" onclick="inserir(1)">1</button>
+    <button class="btn click" onclick="inserir(2)">2</button>
+    <button class="btn click" onclick="inserir(3)">3</button>
+</div>
+
+<div class="preto2">
+    <button class="btn click" onclick="inserir(4)">4</button>
+    <button class="btn click" onclick="inserir(5)">5</button>
+    <button class="btn click" onclick="inserir(6)">6</button>
+</div>
+
+<div class="preto3">
+     <button class="btn click" onclick="inserir(7)">7</button>
+    <button class="btn click" onclick="inserir(8)">8</button>
+    <button class="btn click" onclick="inserir(9)">9</button>
+</div>
+
+<div class="preto4">
+    <button class="btn click" onclick="inserir(0)">0</button>
+</div>
+    <div class="teclado2">
+
+<button class="branco click">branco</button>
+<button class="laranja click"onclick="corrigo()">corrigir</button>
+<button class="verde"onclick="votar()">confirmar</button>
+
+
+</div>
+</div>
+
+
 </body>
 </html>
