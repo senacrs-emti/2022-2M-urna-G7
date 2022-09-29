03b0eee48eb2717e15c90a50bed64056975e088a
DouglasOliveira <douglasoliveiradaluz10@gmail.com>
2022-09-29 12:20:40 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -15,37 +15,39 @@
 
 <div class="retangulo1">
     <div class="retangulo2">
-        <div class="retangulo3"></div>
+        <div class="retangulo3" ></div>
     </div>
 </div>
-
-<div class="preto1">
-    <button class="btn click" onclick="inserir(1)">1</button>
-    <button class="btn click" onclick="inserir(2)">2</button>
-    <button class="btn click" onclick="inserir(3)">3</button>
+<div id="imagem" id="imagem">
+    <img src="./img/logo-justica-eleitora-senac.png" alt="">
+</div>
+<div class="preto1" id="pr">
+    <button class="btn preto click" onclick="inserir(1)">1</button>
+    <button class="btn preto click"  onclick="inserir(2)">2</button>
+    <button class="btn preto click"  onclick="inserir(3)">3</button>
 </div>
 
-<div class="preto2">
-    <button class="btn click" onclick="inserir(4)">4</button>
-    <button class="btn click" onclick="inserir(5)">5</button>
-    <button class="btn click" onclick="inserir(6)">6</button>
+<div class="preto2" id="pr2">
+    <button class="btn preto click" onclick="inserir(4)">4</button>
+    <button class="btn preto click" onclick="inserir(5)">5</button>
+    <button class="btn preto click" onclick="inserir(6)">6</button>
 </div>
 
-<div class="preto3">
-     <button class="btn click" onclick="inserir(7)">7</button>
-    <button class="btn click" onclick="inserir(8)">8</button>
-    <button class="btn click" onclick="inserir(9)">9</button>
+<div class="preto3" id="pr3">
+     <button class="btn preto click" onclick="inserir(7)">7</button>
+    <button class="btn preto click" onclick="inserir(8)">8</button>
+    <button class="btn preto click" onclick="inserir(9)">9</button>
 </div>
 
 <div class="preto4">
-    <button class="btn click" onclick="inserir(0)">0</button>
+    <button class="btn preto click" onclick="inserir(0)">0</button>
 </div>
-    <div class="teclado2">
+    <div>
 
 <button class="branco click">branco</button>
 <button class="laranja click"onclick="corrigo()">corrigir</button>
-<button class="verde"onclick="votar()">confirmar</button>
-
+<button class="verde" onclick="votar()">confirmar</button>
+</div>
 
 </div>
 </div>
